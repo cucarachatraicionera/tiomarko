50c8d26209b367529f1fcc25787335b03c0ac7a4
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-03-25 05:11:04 -0500

    corrigiendo pa q marquito seponga happpy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b277925516d47a82"; }
+/******/ 		__webpack_require__.h = function() { return "429733adf962a23a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
